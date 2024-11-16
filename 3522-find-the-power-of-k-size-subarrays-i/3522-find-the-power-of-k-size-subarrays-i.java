944a8e109889b7cd0d32b7d67dcdb44f72ea06ba
ISHAN MISHRA <131025039+ishan423@users.noreply.github.com>
2024-11-16 23:36:23 +0530

    Time: 1 ms (100%), Space: 45.5 MB (46.81%) - LeetHub

--- 3522-find-the-power-of-k-size-subarrays-i/3522-find-the-power-of-k-size-subarrays-i.java
+++ 3522-find-the-power-of-k-size-subarrays-i/3522-find-the-power-of-k-size-subarrays-i.java
@@ -1,51 +1,33 @@
-// class Solution {
-//     public int[] resultsArray(int[] nums, int k) {
-//                 int N = nums.length;
-//                 int [] arr = new int[N-k+1];
-//                 // Arrays.fill(arr,-1);
-//                 for(int i = 0;i<=N-k;i++){
-//                     // x = true;
-//                 Boolean  x = true;
-
-//                     for(int j = 0;j<i+k-1;j++){
-//                         if(nums[j+1]!=nums[j]+1){
-//                             x = false;
-//                             break;
-//                         }
-//                     }
-//                         if(x){
-//                             arr[i] = arr[i + k - 1];
-//                         }else{
-//                             arr[i] = -1;
-//                         }
-
-//                 }        
-//                     return arr;
-//     }
-// }
 class Solution {
-
-    public int[] resultsArray(int[] nums, int k) {
-        int N = nums.length;
-        int[] arr = new int[N - k + 1];
-
-        for (int i= 0; i <= N - k; i++) {
-            boolean x = true;
-
-            for (int j = i; j < i + k - 1; j++) {
-                if (nums[j + 1] != nums[j] + 1) {
-                    x = false;
-                    break;
-                }
-            }
-
-            if (x) {
-                arr[i] = nums[i + k - 1];
-            } else {
-                arr[i] = -1;
+    public int[] resultsArray(int[] nums, int k) 
+    {
+        int n = nums.length;
+        int subArrCount = n-k+1;
+        int[] subArrPower = new int[n-k+1];
+        int inconsecutiveElementCount = 0;
+        for(int i=1; i<k; i++)
+        {
+            if(nums[i] != nums[i-1]+1)
+            {
+                inconsecutiveElementCount++;
             }
         }
-
-        return arr;
+        int start =0, end = k-1;
+        while(end<n)
+        {
+            subArrPower[start] = inconsecutiveElementCount == 0 ? nums[end] : -1;
+            if(start<n-1 && nums[start] != nums[start+1]-1)
+            {
+                inconsecutiveElementCount--;
+            }
+            start++;
+            if(end<n-1 && nums[end+1] != nums[end]+1)
+            {
+                inconsecutiveElementCount++;
+            }
+            end++;
+        }
+        return subArrPower;
+        
     }
 }
