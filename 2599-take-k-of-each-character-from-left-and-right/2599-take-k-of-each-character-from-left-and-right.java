549372f599cc721c525469601d955d964a95a4be
ISHAN MISHRA <131025039+ishan423@users.noreply.github.com>
2024-11-21 01:51:51 +0530

    Time: 9 ms (96.77%), Space: 45.3 MB (63.03%) - LeetHub

--- 2599-take-k-of-each-character-from-left-and-right/2599-take-k-of-each-character-from-left-and-right.java
+++ 2599-take-k-of-each-character-from-left-and-right/2599-take-k-of-each-character-from-left-and-right.java
@@ -1,40 +1,26 @@
 class Solution {
-
     public int takeCharacters(String s, int k) {
-        int[] count = new int[3];
-        int n = s.length();
-
-        // Count total occurrences
-        for (char c : s.toCharArray()) {
-            count[c - 'a']++;
+        int freq[]=new int[3];
+        int n=s.length();
+        for(char ch:s.toCharArray()){
+            freq[ch-'a']++;
         }
-
-        // Check if we have enough characters
-        for (int i = 0; i < 3; i++) {
-            if (count[i] < k) return -1;
+        if(freq[0]<k || freq[1]<k || freq[2]<k){
+            return -1;
         }
-
-        int[] window = new int[3];
-        int left = 0, maxWindow = 0;
-
-        // Find the longest window that leaves k of each character outside
-        for (int right = 0; right < n; right++) {
-            window[s.charAt(right) - 'a']++;
-
-            // Shrink window if we take too many characters
-            while (
-                left <= right &&
-                (count[0] - window[0] < k ||
-                    count[1] - window[1] < k ||
-                    count[2] - window[2] < k)
-            ) {
-                window[s.charAt(left) - 'a']--;
-                left++;
+        int max=Integer.MIN_VALUE;
+        int j=0;
+        int i=0;
+        while(j<n){
+            char ch=s.charAt(j);
+            freq[ch-'a']--;
+            while(i<=j && freq[ch-'a']<k){
+                freq[s.charAt(i)-'a']++;
+                i++;
             }
-
-            maxWindow = Math.max(maxWindow, right - left + 1);
+            max=Math.max(max,j-i+1);
+            j++;
         }
-
-        return n - maxWindow;
+        return n-max;
     }
 }
