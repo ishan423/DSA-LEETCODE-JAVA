5a4b87b9632269d421f324d1d6e2eb11fca4a066
ISHAN MISHRA <131025039+ishan423@users.noreply.github.com>
2024-11-26 23:35:25 +0530

    Time: 1 ms (100%), Space: 45.9 MB (25%) - LeetHub

--- 3189-find-champion-ii/3189-find-champion-ii.java
+++ 3189-find-champion-ii/3189-find-champion-ii.java
@@ -1,27 +1,27 @@
 class Solution {
-    public int findChampion(int n, int[][] e) {
-        int indegree = 0;
-        int [] arr = new int[n];
-        Arrays.fill(arr,0);
-            for(int [] e1:e){
+    public int findChampion(int n, int[][] e1) {
+        int teams[] = new int[n];
 
-            
-              System.out.println(arr[e1[1]]++ + " " + e1[1]);
+        for(int e[] : e1)
+        {
+            teams[e[1]]++;
+        }
+
+        int result = -1;
+
+        for(int i = 0; i < n; i++)
+        {
+            if(teams[i] == 0 && result != -1)
+            {
+                return -1;
             }
-                        
-            int ans =-1;
-            
-            int cnt =  0;
-            for(int i = 0;i<n;i++){
-                if(arr[i] == 0){
-                    cnt++;
-                    ans = i;
-
-                }
+            else if(teams[i] == 0)
+            {
+                result = i;
             }
+        }
 
-
-        return cnt>1?-1:ans ;
+        return result;
 
 
     }
