fb14281e5a43f1ca27a2725a36d06a7d1d4aafb8
ISHAN MISHRA <131025039+ishan423@users.noreply.github.com>
2024-12-01 22:05:49 +0530

    Time: 3 ms (33.07%), Space: 42.5 MB (88.24%) - LeetHub

--- 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.java
+++ 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.java
@@ -3,16 +3,46 @@ class Solution {
         Arrays.sort(arr);
         int db = 0;
         for(int i = 0;i<arr.length;i++){
-           for(int j = 0;j<arr.length-1;j++){
-            if( i!=j &&arr[i] == 2*arr[j] ){
-                return true;
-            }
+        //    for(int j = 0;j<arr.length-1;j++){
+            // if( i!=j &&arr[i] == 2*arr[j] ){
+            //     return true;
+            // }
             // else{
             //     return false;
             // }
-           }
+        //    }
+
+             int target = 2*arr[i];
+
+             int isvalid = valid(arr,target);
+             if(isvalid !=i && isvalid>=0){
+                return true;
+             }
+            //  else{
+            //     return false;
+            //  }
+
         }
 
             return false;
+    }
+    public static int  valid(int [] arr, int x1 ){
+              int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right) {
+           
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] == x1) {
+                return mid;
+            } else if (arr[mid] < x1) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return -1;  
     }
 }
