9565a9fe79da9aad531e2d3f523d3bd06c4310c3
ISHAN MISHRA <131025039+ishan423@users.noreply.github.com>
2024-12-04 00:02:21 +0530

    Time: 22 ms (85.57%), Space: 78.1 MB (96.21%) - LeetHub

--- 2232-adding-spaces-to-a-string/2232-adding-spaces-to-a-string.java
+++ 2232-adding-spaces-to-a-string/2232-adding-spaces-to-a-string.java
@@ -2,7 +2,7 @@ class Solution {
     public String addSpaces(String s, int[] spaces) {
         int cnt = 0;
         StringBuilder sb = new StringBuilder();
-
+             sb.ensureCapacity(s.length() + spaces.length);// this reallocate the no of maximum character n a string which is effiecient as due to this the string builder does not need to allocate memeory every time it is called
         for(int i = 0;i<s.length();i++)
         {
             if(cnt <spaces.length && i == spaces[cnt] ){
