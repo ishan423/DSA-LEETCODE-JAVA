0068df757bbd34d7912d1786abcce99bd69b9b44
ISHAN MISHRA <131025039+ishan423@users.noreply.github.com>
2024-12-13 00:40:36 +0530

    Time: 6 ms (65.07%), Space: 42.7 MB (21.79%) - LeetHub

--- 2692-take-gifts-from-the-richest-pile/2692-take-gifts-from-the-richest-pile.java
+++ 2692-take-gifts-from-the-richest-pile/2692-take-gifts-from-the-richest-pile.java
@@ -8,25 +8,21 @@ class Solution {
     }
     int j = 0;
     for(int i = 0 ;i<k;i++){
-        // while(j<=k){
+        
          max = pq.poll();
 
         double sq1 = Math.sqrt(Math.ceil(max));
-        // double sq1 = ;
+        
         int x = (int)sq1;
         pq.offer(x);
        
-    //    j++;
-        // }
+   
     }
     long max1 = 0;
     while(!pq.isEmpty()){
         max1+= pq.poll();
     }
-    // for(int i = 0;i<)
-        
-
-    // System.out.println(pq.peek());
+ 
     
     return max1;}
 }
