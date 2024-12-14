e315496280ef692faeb17e32aac11cb8ff06e4ed
ISHAN MISHRA <131025039+ishan423@users.noreply.github.com>
2024-12-15 01:49:16 +0530

    Time: 231 ms (7.08%), Space: 58 MB (70.4%) - LeetHub

--- 2868-continuous-subarrays/2868-continuous-subarrays.java
+++ 2868-continuous-subarrays/2868-continuous-subarrays.java
@@ -1,30 +1,24 @@
 class Solution {
-
     public long continuousSubarrays(int[] nums) {
-        // TreeMap to maintain sorted frequency map of current window
-        TreeMap<Integer, Integer> freq = new TreeMap<>();
-        int left = 0, right = 0;
-        int n = nums.length;
-        long count = 0; // Total count of valid subarrays
+        long count = 0;
+        int start = 0;
+        int end = 0;
+                  
+        PriorityQueue<Integer> minH = new PriorityQueue<>();
+        PriorityQueue<Integer> maxH = new PriorityQueue<>((a, b) -> b - a);
 
-        while (right < n) {
-            // Add current element to frequency map
-            freq.put(nums[right], freq.getOrDefault(nums[right], 0) + 1);
-
-            // While window violates the condition |nums[i] - nums[j]| ≤ 2
-            // Shrink window from left
-            while (freq.lastEntry().getKey() - freq.firstEntry().getKey() > 2) {
-                // Remove leftmost element from frequency map
-                freq.put(nums[left], freq.get(nums[left]) - 1);
-                if (freq.get(nums[left]) == 0) {
-                    freq.remove(nums[left]);
-                }
-                left++;
+        while (end < nums.length) {
+            maxH.offer(nums[end]);
+            minH.offer(nums[end]);
+            while (maxH.peek() - minH.peek()>2)
+            {
+                maxH.remove(nums[start]);
+                minH.remove(nums[start]);
+                start++;
             }
 
-            // Add count of all valid subarrays ending at right
-            count += right - left + 1;
-            right++;
+            count+= end*1L-start*1L+ 1*1L;
+            end++;
         }
 
         return count;
