9a9577d66070d6f35ef69b6be7e5c7fd75c89dc2
ISHAN MISHRA <131025039+ishan423@users.noreply.github.com>
2024-12-18 00:28:59 +0530

    Time: 19 ms (74.58%), Space: 45.7 MB (50.42%) - LeetHub

--- 2300-construct-string-with-repeat-limit/2300-construct-string-with-repeat-limit.java
+++ 2300-construct-string-with-repeat-limit/2300-construct-string-with-repeat-limit.java
@@ -1,43 +1,34 @@
 class Solution {
-
     public String repeatLimitedString(String s, int repeatLimit) {
-        Map<Character, Integer> freq = new HashMap<>();
-        for (char ch : s.toCharArray()) {
-            freq.put(ch, freq.getOrDefault(ch, 0) + 1);
+        int[] count = new int[26];
+        for(char c:s.toCharArray()){
+            ++count[c-'a'];
         }
-
-        PriorityQueue<Character> maxHeap = new PriorityQueue<>((a, b) ->
-            Character.compare(b, a)
-        );
-
-        for (char ch : freq.keySet()) {
-            maxHeap.offer(ch);
-        }
-
-        StringBuilder result = new StringBuilder();
-
-        while (!maxHeap.isEmpty()) {
-            char ch = maxHeap.poll();
-            int count = freq.get(ch);
-
-            int use = Math.min(count, repeatLimit);
-            for (int i = 0; i < use; i++) {
-                result.append(ch);
+        StringBuilder sb = new StringBuilder();
+        int prev=-1;
+        for(int i=25;i>=0;i--){
+            int len=repeatLimit;
+            if(count[i]<=0){
+                continue;
             }
-
-            freq.put(ch, count - use);
-
-            if (freq.get(ch) > 0 && !maxHeap.isEmpty()) {
-                char nextCh = maxHeap.poll();
-                result.append(nextCh);
-                freq.put(nextCh, freq.get(nextCh) - 1);
-                if (freq.get(nextCh) > 0) {
-                    maxHeap.offer(nextCh);
-                }
-                maxHeap.offer(ch);
+            if(prev!=-1){
+                sb.append((char)(i+'a'));
+                --count[i];
+                i=prev+1;
+                prev=-1;
+                continue;
+            }
+            while(count[i]>0 && len>0){
+                --count[i];
+                --len;
+                sb.append((char)(i+'a'));
+            }
+            if(count[i]>0){
+                prev=i;
+            }else{
+                prev=-1;
             }
         }
-
-        return result.toString();
+        return sb.toString();
     }
 }
