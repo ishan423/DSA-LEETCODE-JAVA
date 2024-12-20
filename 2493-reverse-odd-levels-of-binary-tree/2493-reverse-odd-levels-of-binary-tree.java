e391e793d7134866fda02b538ff797f9f8a2f926
ISHAN MISHRA <131025039+ishan423@users.noreply.github.com>
2024-12-21 01:15:20 +0530

    Time: 10 ms (34.26%), Space: 48.8 MB (38.92%) - LeetHub

--- 2493-reverse-odd-levels-of-binary-tree/2493-reverse-odd-levels-of-binary-tree.java
+++ 2493-reverse-odd-levels-of-binary-tree/2493-reverse-odd-levels-of-binary-tree.java
@@ -1,26 +1,62 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-
     public TreeNode reverseOddLevels(TreeNode root) {
-        traverseDFS(root.left, root.right, 1);
+        Queue<TreeNode> queue = new LinkedList<>();
+
+        queue.add(root);
+
+        int level = 0;
+
+        while (!queue.isEmpty()) {
+            List<TreeNode> currLevel = new ArrayList<>();
+
+            int currentSize = queue.size();
+
+            for (int i = 0; i < currentSize; i++) {
+                TreeNode currentNode = queue.remove();
+
+                currLevel.add(currentNode);
+
+                if (currentNode.left != null) {
+                    queue.add(currentNode.left);
+                }
+
+                if (currentNode.right != null) {
+                    queue.add(currentNode.right);
+                }
+            }
+
+            if (level % 2 == 1) {
+                int left = 0;
+                int right = currLevel.size() - 1;
+
+                while (left < right) {
+                    int temp = currLevel.get(left).val;
+                    currLevel.get(left).val = currLevel.get(right).val;
+                    currLevel.get(right).val = temp;
+
+                    left++;
+                    right--;
+                }
+            }
+
+            level++;
+        }
+
         return root;
-    }
-
-    private void traverseDFS(
-        TreeNode leftChild,
-        TreeNode rightChild,
-        int level
-    ) {
-        if (leftChild == null || rightChild == null) {
-            return;
-        }
-        //If the current level is odd, swap the values of the children.
-        if (level % 2 == 1) {
-            int temp = leftChild.val;
-            leftChild.val = rightChild.val;
-            rightChild.val = temp;
-        }
-
-        traverseDFS(leftChild.left, rightChild.right, level + 1);
-        traverseDFS(leftChild.right, rightChild.left, level + 1);
     }
 }
