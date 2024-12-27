c4b30ebb983c5e287571e75a6931ad16a6e17f72
ISHAN MISHRA <131025039+ishan423@users.noreply.github.com>
2024-12-28 00:18:53 +0530

    Time: 3 ms (97.03%), Space: 53.4 MB (61.35%) - LeetHub

--- 1063-best-sightseeing-pair/1063-best-sightseeing-pair.java
+++ 1063-best-sightseeing-pair/1063-best-sightseeing-pair.java
@@ -1,23 +1,20 @@
 class Solution {
+    public int maxScoreSightseeingPair(int[] v) {
+        int res = 0;
+        int max = v[0]; 
+        int i=0;
+        for(int j=1;j<v.length;j++){
+            int temp = v[j]+j;
+            int tres = v[i] + v[j] +i-j;
+            if(max < temp){
+               max = v[j]+j;
+               i=j;
+            }
+            if(tres > res){
+                res = tres;
+            }
+        }     
+        return res;
 
-    public int maxScoreSightseeingPair(int[] values) {
-        int n = values.length;
-
-        // The left score is initially just the value of the first element.
-        int maxLeftScore = values[0];
-
-        int maxScore = 0;
-
-        for (int i = 1; i < n; i++) {
-            int currentRightScore = values[i] - i;
-            // Update the maximum score by combining the best left score so far with the current right score.
-            maxScore = Math.max(maxScore, maxLeftScore + currentRightScore);
-
-            int currentLeftScore = values[i] + i;
-            // Update the maximum left score up to the current index.
-            maxLeftScore = Math.max(maxLeftScore, currentLeftScore);
-        }
-
-        return maxScore;
     }
 }
