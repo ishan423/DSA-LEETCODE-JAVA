b31e6c114c5fc5eb88d35820a705ba37734f489d
ISHAN MISHRA <131025039+ishan423@users.noreply.github.com>
2025-01-12 00:35:06 +0530

    Time: 5 ms (66.55%), Space: 45.3 MB (61.15%) - LeetHub

--- 1502-construct-k-palindrome-strings/1502-construct-k-palindrome-strings.java
+++ 1502-construct-k-palindrome-strings/1502-construct-k-palindrome-strings.java
@@ -1,27 +1,18 @@
 class Solution {
-
     public boolean canConstruct(String s, int k) {
-        // Handle edge cases
-        if (s.length() < k) return false;
-        if (s.length() == k) return true;
-
-        // Initialize frequency array and oddCount
-        int[] freq = new int[26];
-        int oddCount = 0;
-
-        // Increment the value of the index corresponding to the current character
-        for (char chr : s.toCharArray()) {
-            System.out.println(freq[chr - 'a']++);
+        if (k > s.length()) {
+            return false;
         }
-
-        // Count the number of characters that appear an odd number of times in s
-        for (int count : freq) {
-            if (count % 2 == 1) {
-                oddCount++;
+        int[] counts = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            counts[s.charAt(i) - 'a']++;
+        }
+        int numOdds = 0;
+        for (int i : counts) {
+            if (i % 2 == 1) {
+                numOdds++;
             }
         }
-
-        // Return if the number of odd frequencies is less than or equal to k
-        return oddCount <= k;
+        return numOdds <= k;
     }
 }
